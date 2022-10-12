fd99131f04b2eacc07ae83acf49214b4faf7ef1e
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-10-12 16:13:36 +0300

    first commit

---
+++ harwey-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="#">About Us</a>
+      <a href="#">For You</a>
+      <a href="#">Service</a>
+      <a href="#">Blog</a>
+      <a href="#">Vlog</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+};
+export default Navbar;
